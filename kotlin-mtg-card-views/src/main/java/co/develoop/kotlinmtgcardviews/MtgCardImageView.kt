4cb5b3254b5a53f8ad11b6575ca50b6e34bf0259
Adrián Santalla <adrian.santalla@gmail.com>
2017-08-07 14:25:56 +0200

    Added the functionality to zoomIn/zoomOut to the MtgCardImageView

--- kotlin-mtg-card-views/src/main/java/co/develoop/kotlinmtgcardviews/MtgCardImageView.kt
+++ kotlin-mtg-card-views/src/main/java/co/develoop/kotlinmtgcardviews/MtgCardImageView.kt
@@ -5,6 +5,10 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.widget.ImageView
 import co.develoop.kotlinmtgcardviews.transformation.RoundedCornersTransformation
 import com.squareup.picasso.Picasso
@@ -13,14 +17,17 @@ import io.magicthegathering.kotlinsdk.api.MtgCardApiClient
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
+
 class MtgCardImageView : ImageView {
 
     var minWidth: Int = 669
     var minHeight: Int = 930
-    var cardRadius: Float = 7f
+    var cardRadius: Float = 10f
     var cardBackRadius: Float = 26f
 
     lateinit var target: Target
+
+    var isZoomed: Boolean = false
 
     constructor(context: Context) : super(context) {
         init(context, null)
@@ -32,6 +39,20 @@ class MtgCardImageView : ImageView {
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init(context, attrs)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event!!.action == MotionEvent.ACTION_UP) {
+            if (isZoomed) {
+                zoomOut(this)
+                isZoomed = false
+            } else {
+                zoomIn(this)
+                isZoomed = true
+            }
+        }
+
+        return true
     }
 
     private fun init(context: Context, attrs: AttributeSet?) {
@@ -86,5 +107,27 @@ class MtgCardImageView : ImageView {
         val source: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_mtg_card_back)
         val image: Bitmap = Bitmap.createScaledBitmap(source, minWidth, minHeight, false)
         setImageBitmap(RoundedCornersTransformation.applyRoundedCornersTransformationToBitmap(image, cardBackRadius))
+    }
+
+    private fun zoomIn(v: View) {
+        val anim = ScaleAnimation(
+                1f, 1.5f, // Start and end values for the X axis scaling
+                1f, 1.5f, // Start and end values for the Y axis scaling
+                Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
+                Animation.RELATIVE_TO_SELF, 0.5f) // Pivot point of Y scaling
+        anim.fillAfter = true // Needed to keep the result of the animation
+        anim.duration = 300
+        v.startAnimation(anim)
+    }
+
+    private fun zoomOut(v: View) {
+        val anim = ScaleAnimation(
+                1.5f, 1f, // Start and end values for the X axis scaling
+                1.5f, 1f, // Start and end values for the Y axis scaling
+                Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
+                Animation.RELATIVE_TO_SELF, 0.5f) // Pivot point of Y scaling
+        anim.fillAfter = true // Needed to keep the result of the animation
+        anim.duration = 300
+        v.startAnimation(anim)
     }
 }
