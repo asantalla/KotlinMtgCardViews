97c65293a7c12f854a727b968d375338282ba128
Adrián Santalla <adrian.santalla@gmail.com>
2017-08-07 21:32:02 +0200

    Modified the way to translate and scale the image view

--- kotlin-mtg-card-views/src/main/java/co/develoop/kotlinmtgcardviews/MtgCardImageView.kt
+++ kotlin-mtg-card-views/src/main/java/co/develoop/kotlinmtgcardviews/MtgCardImageView.kt
@@ -1,5 +1,8 @@
 package co.develoop.kotlinmtgcardviews
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -7,8 +10,6 @@ import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.ScaleAnimation
 import android.widget.ImageView
 import co.develoop.kotlinmtgcardviews.transformation.RoundedCornersTransformation
 import com.squareup.picasso.Picasso
@@ -24,6 +25,10 @@ class MtgCardImageView : ImageView {
     var minHeight: Int = 930
     var cardRadius: Float = 10f
     var cardBackRadius: Float = 26f
+    var zoomFactor: Float = 1.5f
+
+    var xDest: Float? = 0f
+    var yDest: Float? = 0f
 
     lateinit var target: Target
 
@@ -61,7 +66,8 @@ class MtgCardImageView : ImageView {
         val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.MtgCardImageView, 0, 0)
 
         val multiverseId: Int = attributes.getInteger(R.styleable.MtgCardImageView_card_multiverseId, -1)
-        val scaleFactor = attributes.getFloat(R.styleable.MtgCardImageView_card_scaleFactor, 1f)
+        val scaleFactor = attributes.getFloat(R.styleable.MtgCardImageView_card_scale, 1f)
+        zoomFactor = attributes.getFloat(R.styleable.MtgCardImageView_card_zoom, 1.5f)
 
         minWidth = (minWidth.toFloat() * scaleFactor).toInt()
         minHeight = (minHeight.toFloat() * scaleFactor).toInt()
@@ -109,25 +115,33 @@ class MtgCardImageView : ImageView {
         setImageBitmap(RoundedCornersTransformation.applyRoundedCornersTransformationToBitmap(image, cardBackRadius))
     }
 
-    private fun zoomIn(v: View) {
-        val anim = ScaleAnimation(
-                1f, 1.5f, // Start and end values for the X axis scaling
-                1f, 1.5f, // Start and end values for the Y axis scaling
-                Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
-                Animation.RELATIVE_TO_SELF, 0.5f) // Pivot point of Y scaling
-        anim.fillAfter = true // Needed to keep the result of the animation
-        anim.duration = 300
-        v.startAnimation(anim)
+    private fun zoomIn(view: View) {
+        val locationOnScreen: IntArray = IntArray(2)
+        getLocationOnScreen(locationOnScreen)
+
+        xDest = (resources.displayMetrics.widthPixels / 2 - view.measuredWidth / 2 - locationOnScreen[0]).toFloat()
+        yDest = (resources.displayMetrics.heightPixels / 2 - view.measuredHeight / 2 - locationOnScreen[1]).toFloat()
+
+        val translateX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, xDest as Float)
+        val translateY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, yDest as Float)
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, zoomFactor)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, zoomFactor)
+
+        val animator: ValueAnimator = ObjectAnimator.ofPropertyValuesHolder(view, translateX, translateY, scaleX, scaleY)
+        animator.duration = 200
+        animator.start()
+
+        requestFocusFromTouch()
     }
 
-    private fun zoomOut(v: View) {
-        val anim = ScaleAnimation(
-                1.5f, 1f, // Start and end values for the X axis scaling
-                1.5f, 1f, // Start and end values for the Y axis scaling
-                Animation.RELATIVE_TO_SELF, 0.5f, // Pivot point of X scaling
-                Animation.RELATIVE_TO_SELF, 0.5f) // Pivot point of Y scaling
-        anim.fillAfter = true // Needed to keep the result of the animation
-        anim.duration = 300
-        v.startAnimation(anim)
+    private fun zoomOut(view: View) {
+        val translateX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, 0f)
+        val translateY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, 0f)
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f)
+
+        val animator: ValueAnimator = ObjectAnimator.ofPropertyValuesHolder(view, translateX, translateY, scaleX, scaleY)
+        animator.duration = 200
+        animator.start()
     }
 }
